ade047a11ce171ccd9d904e1e296ee092bbf3e70
Maceina <maceinaaudrius@gmail.com>
2020-12-13 16:52:29 +0200

    All tasks done

---
+++ task1.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def projection(u, w):
+    return (np.vdot(u, w) / (np.linalg.norm(w)) ** 2) * w
+
+
+def qr_decomposition(a):
+    # Get dimension of matrix.
+    n = len(a)
+    # Create a copy of the matrix.
+    cp = a.copy()
+
+    # Initialize 2 matrices Q and R.
+    Q = np.zeros(shape=(n, n))
+    R = np.zeros(shape=(n, n))
+
+    for i in range(n):
+        u = a[:, i]
+        u = u.astype('float32')
+        w = u
+        # Apply GS method.
+        for k in range(i):
+            u -= projection(w, Q[:, k])
+        Q[:, i] = normalize(u)
+        # Fill R at correct position.
+        for j in range(i + 1):
+            R[j, i] = np.vdot(Q[:, j], cp[:, i])
+
+    # Return results.
+    return Q, R
+
+
+# Solves Ax=B
+def solve(q, r, b):
+    y = np.dot(q.T, b)
+    n = len(r)
+    x = np.zeros((4, 1))
+    for i in range(n - 1, -1, -1):
+        sum = 0
+        for j in range(len(r[i]) - 1, i, -1):
+            sum += r[i, j] * x[j]
+        x[i] = (y[i] - sum) / r[i, i]
+
+    return x
+
+
+# Solves system using numpy
+def check_with_numpy(a, b):
+    q, r = np.linalg.qr(a)
+    _y = np.dot(q.T, b)
+    return np.linalg.solve(r, _y)
+
+
+# Checks if all x satisfy all equations in the system by inserting them in each equation
+def check_x(a, b, x):
+    counter = 0
+    for i in range(0, len(a)):
+        _sum = 0
+        for j in range(0, len(a[i])):
+            _sum += a[i, j] * x[j]
+
+        if abs(b[i] - _sum) > eps:
+            counter += 1
+        else:
+            return False
+
+    return True
+
+
+# A = np.array([[1, 1, 1, 1],
+#               [1, -1, -1, 1],
+#               [2, 1, -1, 2],
+#               [3, 1, 2, -1]])
+#
+# B = np.array([2, 0, 9, 7])
+
+A = np.array([[3, 1, -1, 1],
+              [1, -2, 3, 1],
+              [2, -9, 5, 2],
+              [1, -7, 2, 1]])
+
+B = np.array([27, 24, 27, 3])
+
+eps = 1e-10
+
+Q, R = qr_decomposition(A.copy())
+y = np.dot(Q.T, B)
+temp = np.linalg.solve(R, y)
+x = solve(Q, R, B.copy())
+print(f"x = \n{x}")
+
+x_correct = check_x(A, B, x)
+print(f"x satisfy all equations in the system: {x_correct}\n")
+
+x_check = check_with_numpy(A, B)
+print(f"Solved with numpy: x = {x_check}")
---
+++ task2-1.py
@@ -0,0 +1,123 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+
+np.warnings.filterwarnings('ignore')
+
+
+# https://matplotlib.org/3.1.0/gallery/mplot3d/surface3d.html
+# https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.contour.html
+
+# Shows z1 function graph
+def show_z1_graph():
+    fig = plt.figure()
+    fig.canvas.set_window_title('Z1')
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(XX, YY, Z1, cmap=cm.coolwarm,
+                           alpha=0.5)
+    surfZ = ax.plot_surface(XX, YY, np.zeros(np.shape(Z1)), antialiased=False, alpha=0.2)
+    cp = ax.contour(X, Y, Z1, levels=0, colors='red')
+    plt.show()
+
+
+# Shows z2 function graph
+def show_z2_graph():
+    fig = plt.figure()
+    fig.canvas.set_window_title('Z2')
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(XX, YY, Z2, cmap=cm.summer,
+                           antialiased=False, alpha=0.5)
+    surf_z = ax.plot_surface(XX, YY, np.zeros(np.shape(Z1)), antialiased=False, alpha=0.2)
+    cp = ax.contour(X, Y, Z2, levels=0, colors='green')
+    plt.show()
+
+
+# Solves system graphically
+def show_f_roots():
+    fig = plt.figure()
+    fig.canvas.set_window_title('Result')
+    ax = fig.gca()
+    ax.grid(color='#C0C0C0', linestyle='-', linewidth=0.5)
+    cp = ax.contour(X, Y, Z1, levels=0, colors='red')
+    cp = ax.contour(X, Y, Z2, levels=0, colors='green')
+    plt.show()
+
+
+# Solves system using newton's method
+def newton(x):
+    ff = f(x)
+    dff = df(x)
+    for i in range(max_iterations):
+        dff = df(x)
+        delta_x, a, b, c = np.linalg.lstsq(-dff, ff)
+
+        x1 = x.reshape(2, 1) + alpha * delta_x
+        ff1 = f([x1[0, 0], x1[1, 0]])
+
+        precision = np.linalg.norm(delta_x) / (np.linalg.norm(x) + np.linalg.norm(delta_x))
+        print(f"Iteration: {i} Precision: {precision}")
+
+        if precision < eps:
+            print(f"Solution: {x}")
+            return x
+        elif i == max_iterations:
+            print(f"Set precision not reached. Last x = {x}")
+            return
+
+        x = np.array([x1[0, 0], x1[1, 0]])
+        ff = ff1
+
+
+# System of nonlinear equations
+# def f(x):
+#     return np.asmatrix([
+#         [x[0] ** 2 + x[1] ** 2 - 2],
+#         [x[0] ** 2 - x[1] ** 2]
+#     ])
+
+
+def f(x):
+    return np.asmatrix([
+        [x[0] ** 2 + 10 * (np.sin(x[0]) + np.cos(x[1])) ** 2 - 10],
+        [(x[1] - 3) ** 2 + x[0] - 8]
+    ])
+
+
+def df(x):
+    return np.asmatrix([
+        [2 * x[0] + 20 * (np.sin(x[0]) + np.cos(x[1])) * np.cos(x[0]),
+         20 * (np.sin(x[0]) + np.cos(x[1])) * (-np.sin(x[1]))],
+        [1, 2 * (x[1] - 3)]
+    ])
+
+
+# System of nonlinear equations derivative
+# def df(x):
+#     return np.asmatrix([
+#         [2 * x[0], 2 * x[1]],
+#         [2 * x[0], -2 * x[1]]
+#     ])
+
+
+# Used for showing graphs
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+XX, YY = np.meshgrid(X, Y)
+
+Z1 = XX ** 2 + 10 * (np.sin(XX) + np.cos(YY)) ** 2 - 10
+Z2 = (YY - 3) ** 2 + XX - 8
+
+show_z1_graph()
+show_z2_graph()
+
+show_f_roots()
+
+alpha = 1
+max_iterations = 200
+eps = 1e-10
+initial_x = np.array([1, 1])  # Initial guess
+
+# Solves system and checks with f function
+result = newton(initial_x)
+print(f(result))
